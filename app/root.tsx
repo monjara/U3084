86ba0b2c0de241af0c45dddb9add63be217439e6
monjara <yaginumee@gmail.com>
2022-04-01 00:57:51 +0900

    add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -5,18 +5,23 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "remix";
-import type { MetaFunction } from "remix";
+} from 'remix'
+import type { MetaFunction } from 'remix'
+import styles from './tailwind.css'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+})
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: styles },
+]
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <Meta />
         <Links />
@@ -28,5 +33,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
